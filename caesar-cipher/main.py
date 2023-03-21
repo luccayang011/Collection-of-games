c5c57f0bf8b6465e113a5653ca88e6fee473a177
Ying Yang <105765526+luccayang011@users.noreply.github.com>
2023-03-20 22:47:23 -0400

    Create main.py

---
+++ caesar-cipher/main.py
@@ -0,0 +1,30 @@
+import art
+print(art.logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = (position + shift_amount) % 26
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+restart_response = True # initial condition to keep the while loop works
+while restart_response == True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  print("")
+  # ask users if they want to continue or not
+  restart = input("Do you want to restart the cipher program? type 'yes' or 'no'\n").lower()
+  if restart == "no":
+    restart_response = False
